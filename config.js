d84ce3f0499a26571982c713b5b9b746ebf87c81
⚜️ ╰‿╯ MR Pabasara Malinda  ད★࿐ <142970254+CyberPabuZa0@users.noreply.github.com>
2024-10-24 15:25:14 +0530

    Update config.js

--- config.js
+++ config.js
@@ -22,7 +22,7 @@ const settingsMenu = [
   ]
 DATABASE_URL = process.env.DATABASE_URL === undefined ? './bot.db' : process.env.DATABASE_URL;
 DEBUG = process.env.DEBUG === undefined ? false : convertToBool(process.env.DEBUG);
-if (!(process.env.SESSION || process.env.SESSION_ID)) throw new Error("No session found, add session before starting bot")
+if (!(process.env.SESSION || process.env.SESSION_ID)) throw new Error("Raganork~ab4dc903ad30ab0a01554d01a96a9bdd:796c7477747569654a79593d")
 module.exports = {
     VERSION: 'v4.0.0',
     ALIVE: process.env.ALIVE || "https://i.imgur.com/KCnoMM2.jpg Hey {sender}, I'm alive \n Uptime: {uptime}",
